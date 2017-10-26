5cde682ba5880fb4873f2a280cbec644138937ad
Gurkenkater <coregamer89@hotmail.de>
2017-10-26 11:56:57 +0200

    DBConnector progress with writeToDB

--- botconfig-backend/modules/dbconnector.js
+++ botconfig-backend/modules/dbconnector.js
@@ -2,27 +2,51 @@ let MongoClient = require('mongodb').MongoClient;
 let url = 'mongodb://141.19.145.166:27017/mydb';
 let exports = {};
 
-let DBConnector = {
-    ip: '',
-    username: '',
-    password: '',
-}
-let Observer = [];
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    db.createCollection('agent', function(err, res) {
-        if(err) throw err;
-        db.close();
+let DBConnector = 
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.createCollection('agent', function(err, res) {
+            if(err) throw err;
+        });
+        return true;
     });
-});
+let Observer = [];
+let agentCollection = DBConnector.collection('agent');
 
 /**
  * @param {*JSON-Object} data
  * @returns boolean if writing to the Database was sucessfull
  */
-exports.writeToDB = function (data) {
+exports.writeToDB = function (data ) {
     
+    //
+    if(data.botID === undefined) {
+        if(data.key === "create"){
+            agentCollection.insertOne(data);
+
+        }else if(data.key === "update"){
+            let id = data.newValue.id;
+            // Look for entry with this id
+            if (agentCollection.findOne(data.key === botID)) {
+                //Nicht sicher ob hier nur der botID Key gelöscht wird und nicht das ganze JSON-Object
+                agentCollection.deleteOne(data.botID, function(err, res) {
+                    if (err) throw err;
+                }
+            );
+                //Hier fehlt das Feld von dem JSON in welchem die Änderung drin steht.
+                agentCollection.updateOne(data.data.key);
+            }
+            else {
+                agentCollection.insertOne(data);
+            }
+        }
+    }else if(data.intentID === undefined) {
+
+    }else {
+
+    }
+    
+
     return;
 }
 
@@ -31,6 +55,7 @@ exports.writeToDB = function (data) {
  * @returns an JSON-Object with the needed Bot-Information
  */
 function readFromDB(reference) {
+
     return;
 }
 
