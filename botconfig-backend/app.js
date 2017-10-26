f0943f234f6b541b7f214f3608e74b111f868515
Manuel Schwalm <manuel.schwalm@sovanta.com>
2017-10-26 10:14:45 +0200

    Renamed index.js to botconfig.js; Merged

--- botconfig-backend/app.js
+++ botconfig-backend/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index.js');
+var botconfig = require('./routes/botconfig.js');
 
 var app = express();
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', botconfig);
 
 
 module.exports = app;
