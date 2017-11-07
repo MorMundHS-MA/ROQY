cbcc550318216853517dca504c61dbb5e7326610
Manuel Schwalm <manuel.schwalm@sovanta.com>
2017-11-07 16:48:15 +0100

    .

--- botconfig-backend/modules/dbconnector.js
+++ botconfig-backend/modules/dbconnector.js
@@ -3,14 +3,21 @@ let MongoClient = require('mongodb').MongoClient;
 let url = 'mongodb://141.19.145.166:27017/mydb';
 
 /**
+ * This method is for write a new bot or changing intents inside
+ * the Mongo Database
  * @param {*JSON-Object} request
  * @returns boolean if writing to the Database was sucessfull
  */
-
 exports.writeToDB = function (request) {
+
+    let retval = {
+        "message":"",
+        "success":true
+    }
 
     return MongoClient.connect(url, function (err, db) {
         if (err) throw err;
+
         //To create a new Bot
         db.createCollection("botAgents", function (err, res) {
             if (err) throw err;
@@ -21,7 +28,7 @@ exports.writeToDB = function (request) {
             db.collection("botAgents").insertOne(request.data, function (err, res) {
                 if (err) {
                     console.log('Bot couldnt get inserted! Dont ask me why.');
-                throw err;
+                    throw err;
                 }
                 console.log('Bot sucessfull inserted!');
                 return true;
@@ -36,23 +43,85 @@ exports.writeToDB = function (request) {
                     return false;
                 }
                 if (request.intendID === undefinded) {
-                    //TODO replace found bot with request.data
+                    //TODO delete whole bot and insert new
                     return true;
                 }
                 else {
-                    db.collection("botAgents").findOne(request.botID.intendID, 
-                    function (err, res) {
-                        if (err) {
-                            console.log('A bot with such an Intent ID couldnt be found.');
-                            return false;
-                        }
-                        else {
-                            //TODO replace old intent with request.data
-                            return true;
-                        }
-                    });
+                    db.collection("botAgents").findOne(request.botID.intendID,
+                        function (err, res) {
+                            if (err) {
+                                console.log('A bot with such an Intent ID couldnt be found.');
+                                return false;
+                            }
+                            else {
+                                //TODO replace old intent with request.data
+                                return true;
+                            }
+                        });
                 }
             });
         }
     });
+}
+
+/**
+ * This method is for getting a whole bot or reading intents out of the
+ * Mongo Database
+ */
+exports.readFromDB = function (request) {
+
+    return new Promise(function(resolve){
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+
+            if(request.botID === undefined) {
+                return db.collection("botAgents").find({}).toArray(function(err, res) {
+                    resolve(res);
+                });
+
+            }
+
+            //If the bot ID is set and intents from a bot are wanted
+            else {
+                db.collection("botAgents").findOne(request.botID, function (err, res) {
+                    if (err) {
+                        console.log('A bot with such an ID can not be found!');
+                        //returns an empty JSON-Object
+                        resolve({});
+                    }
+
+                    else {
+                        //If only one bot is wanted
+                        if(request.intendID === undefined) {
+                            resolve(db.collection("botAgents").findOne(request.botID));
+                        }
+
+                        else {
+                            db.collection("botAgents").findOne(request.botID.intendID,
+                                function (err, res) {
+                                    if (err) {
+                                        //returns an empty JSON-Object
+                                        resolve({});
+                                    }
+                                    else {
+                                        resolve(db.collection("botAgents").findOne(request.botID.intendID));
+                                    }
+                                });
+                        }
+                    }
+                });
+            }
+        });
+    });
+}
+
+
+
+exports.readMultipleFromDB = function(request){
+    let retval = [];
+    for(let i = 0; i<request.length; i++){
+        let response = exports.readFromDB(request[i]);
+        retval.push(response);
+    }
+    return retval;
 }
