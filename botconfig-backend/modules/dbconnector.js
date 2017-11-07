eb372b531f66e36c9d06af0cd085861375d6dacf
Gurkenkater <coregamer89@hotmail.de>
2017-11-07 12:01:38 +0100

    writeToDB progress, insert new bot works!

--- botconfig-backend/modules/dbconnector.js
+++ botconfig-backend/modules/dbconnector.js
@@ -2,36 +2,57 @@ let MongoClient = require('mongodb').MongoClient;
 
 let url = 'mongodb://141.19.145.166:27017/mydb';
 
-cexports = {};
-
-let agentCollection = DBConnector.collection('agent');
-
 /**
- * @param {*JSON-Object} data
+ * @param {*JSON-Object} request
  * @returns boolean if writing to the Database was sucessfull
  */
 
-cexports.writeToDB = function (data) {
+exports.writeToDB = function (request) {
 
     MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
         //To create a new Bot
-        if (data.botID === undefined) {
-            db.collection("botAgents").insertOne(data, function (err, res) {
-                if (err) throw err;
+        db.createCollection("botAgents", function (err, res) {
+            if (err) throw err;
+        });
+
+        //To create an complete new bot, if no botID is set
+        if (request.botID === undefined) {
+            db.collection("botAgents").insertOne(request.data, function (err, res) {
+                if (err) {
+                    console.log('Bot couldnt get inserted! Dont ask me why.');
+                throw err;
+                }
+                console.log('Bot sucessfull inserted!');
                 return true;
             });
         }
 
+        //If something has to get changed on a spezific bot with a bot ID
         else {
-            var newIntend = { $set: { intendID: data.newIntend } };
-            db.collection("botAgents").updateOne(data.intendID, newIntend,
-                function (err, res) {
-                    if (err) {
-                        console.log("Your bot does not have such an intent.");
-                        return false;
-                    }
+            db.collection("botAgents").findOne(request.botID, function (err, res) {
+                if (err) {
+                    console.log('Bot with such a bot ID couldnt be found!');
+                    return false;
+                }
+                if (request.intendID === undefinded) {
+                    //TODO replace found bot with request.data
                     return true;
-                });
+                }
+                else {
+                    db.collection("botAgents").findOne(request.botID.intendID, 
+                    function (err, res) {
+                        if (err) {
+                            console.log('A bot with such an Intent ID couldnt be found.');
+                            return false;
+                        }
+                        else {
+                            //TODO replace old intent with request.data
+                            return true;
+                        }
+                    });
+                }
+            });
         }
     });
 }
