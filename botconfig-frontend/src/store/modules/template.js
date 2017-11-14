bafb3084d47b491b279165ae01cbbbed4a972e1c
madrid95 <madriddajsinani@gmail.com>
2017-11-14 10:04:19 +0100

    eslint

--- botconfig-frontend/src/store/modules/template.js
+++ botconfig-frontend/src/store/modules/template.js
@@ -1,39 +1,37 @@
 import data from '../../api/templateData'
 import * as types from '../mutation-types'
 
-
-//initial state
+// initial state
 const state = {
-    templates: []
+  templates: []
 }
 
 const getters = {
-    getTemplates: state => state.templates,
-    getTemplate: (state, template) => state.templates[state.templates.indexOf(template)]
+  getTemplates: state => state.templates,
+  getTemplate: (state, template) => state.templates[state.templates.indexOf(template)]
 }
-    
-const mutations = {
-    [types.ADD_NEW_TEMPALTE] (state, template) {
-        state.templates.push(template)
-    },
 
-    [types.RECEIVE_TEMPLATES] (state ,{ templates}) {
-        state.templates = templates
-    }
+const mutations = {
+  [types.ADD_NEW_TEMPALTE] (state, template) {
+    state.templates.push(template)
+  },
+
+  [types.RECEIVE_TEMPLATES] (state, { templates }) {
+    state.templates = templates
+  }
 }
-    
+
 const actions = {
-    getAllTemplate ({commit}) {
-        data.getTemplates( templats => {
-            commit(types.RECEIVE_TEMPLATES, { templates })
-        })
-    }
+  getAllTemplate ({commit}) {
+    data.getTemplates(templates => {
+      commit(types.RECEIVE_TEMPLATES, { templates })
+    })
+  }
 }
-    
-    
+
 export default {
-    state,
-    getters,
-    mutations,
-    actions
+  state,
+  getters,
+  mutations,
+  actions
 }
