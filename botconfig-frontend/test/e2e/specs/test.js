787a1f902ec5bbd2a19e143376ac73b8bbd47a2e
MorMund-HSMA <1611229@stud.hs-mannheim.de>
2017-11-15 16:00:59 +0100

    Fixed basic test config

--- botconfig-frontend/test/e2e/specs/test.js
+++ botconfig-frontend/test/e2e/specs/test.js
@@ -11,9 +11,8 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.md-title')
+      .assert.containsText('h1', 'BOT CONFIG')
       .end()
   }
 }
