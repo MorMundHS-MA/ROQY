cc92e0b77bf51497aef52a1bb7e0822ef3d132b8
madrid95 <madriddajsinani@gmail.com>
2017-11-16 11:49:40 +0100

    index

--- botconfig-frontend/src/components/BotHeader.vue
+++ botconfig-frontend/src/components/BotHeader.vue
@@ -1,18 +1,16 @@
 
 <template>
   <div>
-    <md-toolbar class=" md-dense md-raised" style="background-color:#F2F1EF; color: #ff720b;">
+    <md-toolbar class=" md-dense " style="background-color:#F2F1EF; color: #ff720b;">
     <h1 class="md-title">Bot config</h1>
     </md-toolbar>
-    <md-toolbar class=" md-dense md-raised" style="background-color:#F2F1EF; color: #ff720b;">
+    <md-toolbar class=" md-dense " style="background-color:#F2F1EF; color: #ff720b;">
 
       <div id="route">
-        <div >
           <router-link tag="md-button" to="/bots" active-class="active">My Bot</router-link>
           <router-link tag="md-button" to="/template" active-class="active">Bot Template</router-link>
           <router-link tag="md-button" to="/marketplace" active-class="active">Marketplace</router-link>
           <router-link tag="md-button" to="/login" active-class="active">Login</router-link>
-        </div>
       </div>
     </md-toolbar>
     <router-view />
--- botconfig-frontend/src/router/index.js
+++ botconfig-frontend/src/router/index.js
@@ -42,6 +42,8 @@ export default new Router({
       name: 'login',
       component: login
     },
-    { path: '/', redirect: '/bots' }
+    { path: '/',
+      redirect: '/bots'
+    }
   ]
 })
