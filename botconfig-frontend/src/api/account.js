69850858b3d2f811f2b1a29ac17f43f3e696746f
madrid95 <madriddajsinani@gmail.com>
2017-12-07 11:31:54 +0100

    remove setTimeout

--- botconfig-frontend/src/api/account.js
+++ botconfig-frontend/src/api/account.js
@@ -5,18 +5,16 @@ axios.defaults.baseURL = 'http://141.19.142.7:3000'
 export default {
   logIn (account, cb) {
     if (account !== null) {
-      setTimeout(() => {
-        axios.post('/auth', {
-          'username': account.username,
-          'password': account.password
-        })
-        .then(function (response) {
-          cb(account)
-        })
-      .catch(function (error) {
-        console.log(error.message)
+      axios.post('/auth', {
+        'username': account.username,
+        'password': account.password
       })
-      }, 100)
+      .then(function (response) {
+        cb(account)
+      })
+    .catch(function (error) {
+      console.log(error.message)
+    })
     }
   }
 }
