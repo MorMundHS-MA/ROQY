5bf45aa31bd497ad88202f1bd676fde08291244f
soheb0 <1615141@stud.hs-mannheim.de>
2017-12-14 14:29:43 +0100

    t

--- botconfig-frontend/src/components/BotCreator.vue
+++ botconfig-frontend/src/components/BotCreator.vue
@@ -178,6 +178,7 @@ export default {
     width: 80%;
     margin: auto;
     background-color: white;
+    box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)
   }
   .header {
    width: 30%;
