cbfcc8080db6bf6b7a68110b8c6d01fb9753059b
MorMund-HSMA <1611229@stud.hs-mannheim.de>
2017-12-16 16:50:55 +0100

    Added runtime to docker compose to guarantee build

--- bot-runtime/app.js
+++ bot-runtime/app.js
@@ -2,5 +2,10 @@
 
 const runtime = require('./modules/runtime');
 let id = process.env.BOT_ID;
+console.log(id)
+if(id === '0') {
+   process.exit(-1);
+} else {
 setTimeout(() => runtime.start(id), 60000);
+}
 
