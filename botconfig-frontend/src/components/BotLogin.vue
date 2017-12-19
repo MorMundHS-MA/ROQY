e219a47c2510f85d5b2f687ad4c2aea863ca7809
Manuel Schwalm <manuel.schwalm@sovanta.com>
2017-12-19 18:19:26 +0100

    Fixed Logo

--- botconfig-frontend/src/components/BotLogin.vue
+++ botconfig-frontend/src/components/BotLogin.vue
@@ -3,7 +3,7 @@
     <md-layout md-tag="form" novalidate md-align="center">
       <md-layout md-tag="md-card" md-column md-flex="30" md-flex-medium="55" md-flex-small="60" md-flex-xsmall="80" md-large="30">
         <md-card-header>
-          <div><img class="logo" src="../assets/ROQY.png" alt="ROQY"></div>
+          <div><img class="logo" src="../assets/ROQY_l.png" alt="ROQY"></div>
         </md-card-header>
 
         <md-card-content>
